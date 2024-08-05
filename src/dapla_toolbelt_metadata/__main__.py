9e4f9a5c288dc126c16460cda85606f489a3a4e7
Cecilie Seim <68303562+tilen1976@users.noreply.github.com>
2024-08-05 12:57:48 +0200

    remove test

--- src/dapla_toolbelt_metadata/__main__.py
+++ src/dapla_toolbelt_metadata/__main__.py
@@ -1,13 +1,14 @@
 """Command-line interface."""
 
-import click
+# import click
 
 
-@click.command()
-@click.version_option()
-def main() -> None:
-    """Dapla Toolbelt Metadata."""
+# @click.command()
+# @click.version_option()
+# def main() -> None:
+#    """Dapla Toolbelt Metadata."""
+#
 
-
-if __name__ == "__main__":
-    main(prog_name="dapla-toolbelt-metadata")  # pragma: no cover
+# if __name__ == "__main__":
+#    main(prog_name="dapla-toolbelt-metadata")  # pragma: no cover
+#
