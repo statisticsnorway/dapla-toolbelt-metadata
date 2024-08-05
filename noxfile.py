e41fed73d76cf1cd599849967a8b36696a2c54d5
Cecilie Seim <68303562+tilen1976@users.noreply.github.com>
2024-08-05 08:57:50 +0200

    redo files

--- noxfile.py
+++ noxfile.py
@@ -130,6 +130,7 @@ def precommit(session: Session) -> None:
     session.install(
         "pre-commit",
         "pre-commit-hooks",
+        "darglint",
         "ruff",
         "black",
     )
@@ -138,33 +139,22 @@ def precommit(session: Session) -> None:
         activate_virtualenv_in_precommit_hooks(session)
 
 
-@session(python=python_versions[-2:])
+@session(python=python_versions)
 def mypy(session: Session) -> None:
     """Type-check using mypy."""
     args = session.posargs or ["src", "tests"]
     session.install(".")
-    session.install(
-        "mypy",
-        "pytest",
-        "types-setuptools",
-        "pandas-stubs",
-        "pyarrow-stubs",
-        "types-Pygments",
-        "types-colorama",
-        "types-beautifulsoup4",
-        "faker",
-    )
+    session.install("mypy", "pytest")
     session.run("mypy", *args)
     if not session.posargs:
         session.run("mypy", f"--python-executable={sys.executable}", "noxfile.py")
 
 
-@session(python=python_versions[-2:])
+@session(python=python_versions)
 def tests(session: Session) -> None:
     """Run the test suite."""
     session.install(".")
-    session.install("coverage[toml]", "pytest", "pygments", "pytest-mock", "requests-mock", "faker"
-    )
+    session.install("coverage[toml]", "pytest", "pygments")
     try:
         session.run(
             "coverage",
@@ -181,7 +171,7 @@ def tests(session: Session) -> None:
             session.notify("coverage", posargs=[])
 
 
-@session(python=python_versions[-1])
+@session(python=python_versions[0])
 def coverage(session: Session) -> None:
     """Produce the coverage report."""
     args = session.posargs or ["report", "--skip-empty"]
@@ -194,16 +184,15 @@ def coverage(session: Session) -> None:
     session.run("coverage", *args)
 
 
-@session(python=python_versions[-1])
+@session(python=python_versions[0])
 def typeguard(session: Session) -> None:
     """Runtime type checking using Typeguard."""
     session.install(".")
-    session.install("pytest", "typeguard", "pygments", "pytest_mock", "requests_mock", "faker"
-    )
+    session.install("pytest", "typeguard", "pygments")
     session.run("pytest", f"--typeguard-packages={package}", *session.posargs)
 
 
-@session(python=python_versions[-1])
+@session(python=python_versions)
 def xdoctest(session: Session) -> None:
     """Run examples with xdoctest."""
     if session.posargs:
@@ -218,7 +207,7 @@ def xdoctest(session: Session) -> None:
     session.run("python", "-m", "xdoctest", *args)
 
 
-@session(name="docs-build", python=python_versions[-1])
+@session(name="docs-build", python=python_versions[0])
 def docs_build(session: Session) -> None:
     """Build the documentation."""
     args = session.posargs or ["docs", "docs/_build"]
@@ -237,7 +226,7 @@ def docs_build(session: Session) -> None:
     session.run("sphinx-build", *args)
 
 
-@session(python=python_versions[-1])
+@session(python=python_versions[0])
 def docs(session: Session) -> None:
     """Build and serve the documentation with live reloading on file changes."""
     args = session.posargs or ["--open-browser", "docs", "docs/_build"]
