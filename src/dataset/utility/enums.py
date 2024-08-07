12b4864fc9b9683bc82069cc20c48f597d870487
Cecilie Seim <68303562+tilen1976@users.noreply.github.com>
2024-08-07 14:43:02 +0200

    update after pre-commit

--- src/dataset/utility/enums.py
+++ src/dataset/utility/enums.py
@@ -34,7 +34,7 @@ class SupportedLanguages(str, Enum):
     Reference: https://www.iana.org/assignments/language-subtag-registry/language-subtag-registry
     """
 
-    NORSK_BOKMÅL = "nb"  # norwegian character allowed  # noqa: PLC2401
+    NORSK_BOKMÅL = "nb"
     NORSK_NYNORSK = "nn"
     ENGLISH = "en"
 
