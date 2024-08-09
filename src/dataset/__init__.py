6aca28fa049b272696853ad0619aee5ab8ca0979
Cecilie Seim <68303562+tilen1976@users.noreply.github.com>
2024-08-09 10:56:21 +0200

    edit __init__.py

--- src/dataset/__init__.py
+++ src/dataset/__init__.py
@@ -1,9 +1,9 @@
 """Document dataset."""
 
-from .code_list import CodeList
+from datadoc_model import model
+
 from .core import Datadoc
 from .dapla_dataset_path_info import DaplaDatasetPathInfo
 from .model_validation import ObligatoryDatasetWarning
 from .model_validation import ObligatoryVariableWarning
-from .statistic_subject_mapping import StatisticSubjectMapping
 from .utility import enums
