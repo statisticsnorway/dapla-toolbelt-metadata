3557b462e27a868aa9cb8be4e5b2186fb2e1184c
Cecilie Seim <68303562+tilen1976@users.noreply.github.com>
2024-08-13 09:33:45 +0200

    remove LanguageStringEnum

--- src/dataset/utility/enums.py
+++ src/dataset/utility/enums.py
@@ -4,10 +4,6 @@ from __future__ import annotations
 
 from enum import Enum
 
-from datadoc_model import model
-from datadoc_model.model import LanguageStringType
-from datadoc_model.model import LanguageStringTypeItem
-
 
 class DaplaRegion(str, Enum):
     """Dapla platforms/regions."""
@@ -37,90 +33,3 @@ class SupportedLanguages(str, Enum):
     NORSK_BOKMÅL = "nb"
     NORSK_NYNORSK = "nn"
     ENGLISH = "en"
-
-
-class LanguageStringsEnum(Enum):
-    """Enum class for storing LanguageStringType objects.
-
-    Attributes:
-        get_value_for_language:
-    """
-
-    def __init__(
-        self,
-        language_strings: LanguageStringType,
-    ) -> None:
-        """Store the LanguageStringType object for displaying enum values in multiple languages.
-
-        We don't particularly care what the value of the enum is,
-        but when serialised it's convenient and readable to use the
-        name of the enum, so we set the value to be the name.
-        """
-        self._value_ = self.name
-        self.language_strings = language_strings
-
-    def get_value_for_language(
-        self,
-        language: SupportedLanguages,
-    ) -> str | None:
-        """Retrieve the string for the relevant language."""
-        if self.language_strings.root is not None:
-            for item in self.language_strings.root:
-                if item.languageCode == language:
-                    return item.languageText
-        return None
-
-
-class DataSetState(LanguageStringsEnum):
-    """Processing state of a dataset."""
-
-    SOURCE_DATA = LanguageStringType(
-        [
-            LanguageStringTypeItem(
-                languageCode="en",
-                languageText=model.DataSetState.SOURCE_DATA.value,
-            ),
-            LanguageStringTypeItem(languageCode="nn", languageText="KILDEDATA"),
-            LanguageStringTypeItem(languageCode="nb", languageText="KILDEDATA"),
-        ],
-    )
-    INPUT_DATA = LanguageStringType(
-        [
-            LanguageStringTypeItem(
-                languageCode="en",
-                languageText=model.DataSetState.INPUT_DATA.value,
-            ),
-            LanguageStringTypeItem(languageCode="nn", languageText="INNDATA"),
-            LanguageStringTypeItem(languageCode="nb", languageText="INNDATA"),
-        ],
-    )
-    PROCESSED_DATA = LanguageStringType(
-        [
-            LanguageStringTypeItem(
-                languageCode="en",
-                languageText=model.DataSetState.PROCESSED_DATA.value,
-            ),
-            LanguageStringTypeItem(languageCode="nn", languageText="KLARGJORTE DATA"),
-            LanguageStringTypeItem(languageCode="nb", languageText="KLARGJORTE DATA"),
-        ],
-    )
-    STATISTICS = LanguageStringType(
-        [
-            LanguageStringTypeItem(
-                languageCode="en",
-                languageText=model.DataSetState.STATISTICS.value,
-            ),
-            LanguageStringTypeItem(languageCode="nn", languageText="STATISTIKK"),
-            LanguageStringTypeItem(languageCode="nb", languageText="STATISTIKK"),
-        ],
-    )
-    OUTPUT_DATA = LanguageStringType(
-        [
-            LanguageStringTypeItem(
-                languageCode="en",
-                languageText=model.DataSetState.OUTPUT_DATA.value,
-            ),
-            LanguageStringTypeItem(languageCode="nn", languageText="UTDATA"),
-            LanguageStringTypeItem(languageCode="nb", languageText="UTDATA"),
-        ],
-    )
--- tests/dataset/test_dapla_dataset_path_info.py
+++ tests/dataset/test_dapla_dataset_path_info.py
@@ -5,13 +5,13 @@ from dataclasses import dataclass
 from typing import TYPE_CHECKING
 
 import pytest
+from datadoc_model.model import DataSetState
 
 from dataset.dapla_dataset_path_info import ISO_YEAR
 from dataset.dapla_dataset_path_info import ISO_YEAR_MONTH
 from dataset.dapla_dataset_path_info import ISO_YEAR_MONTH_DAY
 from dataset.dapla_dataset_path_info import SSB_BIMESTER
 from dataset.dapla_dataset_path_info import DaplaDatasetPathInfo
-from dataset.utility.enums import DataSetState
 from tests.dataset.constants import TEST_BUCKET_PARQUET_FILEPATH_WITH_SHORTNAME
 from tests.dataset.constants import TEST_PARQUET_FILEPATH
 
