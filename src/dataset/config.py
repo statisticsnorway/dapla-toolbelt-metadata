25789588fc2805b6a9d498d075633a42fad4a64a
Cecilie Seim <68303562+tilen1976@users.noreply.github.com>
2024-08-14 09:11:27 +0200

    Remove config content (#16)
    * change exposing enums
    
    * remove datadoc path
    
    * remove get app name
    
    * remove get port
    
    * remove logging
    
    * remove jupyter hub
    
    * remove codes
    
    * pre commit

--- src/dataset/config.py
+++ src/dataset/config.py
@@ -6,7 +6,6 @@ import logging
 import os
 from pathlib import Path
 from pprint import pformat
-from typing import Literal
 
 from dotenv import dotenv_values
 from dotenv import load_dotenv
@@ -51,60 +50,6 @@ def get_jupyterhub_user() -> str | None:
     return _get_config_item(JUPYTERHUB_USER)
 
 
-def get_datadoc_dataset_path() -> str | None:
-    """Get the path to the dataset."""
-    return _get_config_item("DATADOC_DATASET_PATH")
-
-
-def get_log_level() -> int:
-    """Get the log level."""
-    # Magic numbers as defined in Python's stdlib logging
-    log_levels: dict[str, int] = {
-        "CRITICAL": 50,
-        "ERROR": 40,
-        "WARNING": 30,
-        "INFO": 20,
-        "DEBUG": 10,
-    }
-    if level_string := _get_config_item("DATADOC_LOG_LEVEL"):
-        try:
-            return log_levels[level_string.upper()]
-        except KeyError:
-            return log_levels["INFO"]
-    else:
-        return log_levels["INFO"]
-
-
-def get_log_formatter() -> Literal["simple", "json"]:
-    """Get log formatter configuration."""
-    if (
-        _get_config_item("DATADOC_ENABLE_JSON_FORMATTING") == "True"
-        or get_dapla_region() is not None
-    ):
-        return "json"
-    return "simple"
-
-
-def get_jupyterhub_service_prefix() -> str | None:
-    """Get the JupyterHub service prefix."""
-    return _get_config_item("JUPYTERHUB_SERVICE_PREFIX")
-
-
-def get_app_name() -> str:
-    """Get the name of the app. Defaults to 'Datadoc'."""
-    return _get_config_item("DATADOC_APP_NAME") or "Datadoc"
-
-
-def get_jupyterhub_http_referrer() -> str | None:
-    """Get the JupyterHub http referrer."""
-    return _get_config_item("JUPYTERHUB_HTTP_REFERER")
-
-
-def get_port() -> int:
-    """Get the port to run the app on."""
-    return int(_get_config_item("DATADOC_PORT") or 7002)
-
-
 def get_statistical_subject_source_url() -> str | None:
     """Get the URL to the statistical subject source."""
     return _get_config_item("DATADOC_STATISTICAL_SUBJECT_SOURCE_URL")
@@ -129,23 +74,3 @@ def get_dapla_service() -> DaplaService | None:
 def get_oidc_token() -> str | None:
     """Get the JWT token from the environment."""
     return _get_config_item("OIDC_TOKEN")
-
-
-def get_unit_code() -> int | None:
-    """The code for the Unit Type code list in Klass."""
-    return int(_get_config_item("DATADOC_UNIT_CODE") or 702)
-
-
-def get_measurement_unit_code() -> int | None:
-    """The code for the Measurement Unit code list in Klass."""
-    return int(_get_config_item("DATADOC_MEASUREMENT_UNIT") or 303)
-
-
-def get_organisational_unit_code() -> int | None:
-    """The code for the organisational units code list in Klass."""
-    return int(_get_config_item("DATADOC_ORGANISATIONAL_UNIT_CODE") or 83)
-
-
-def get_data_source_code() -> int | None:
-    """The code for the organisational units code list in Klass."""
-    return int(_get_config_item("DATADOC_DATA_SOURCE_CODE") or 712)
