59ee61f97e4140d4acea0bdf1d0b0d3983254392
Miles Mason Winther <42948872+mmwinther@users.noreply.github.com>
2024-12-04 08:52:48 +0100

    Fix generated vardef client (#70)
    * Update vardef OpenAPI doc
    
    * Generate vardef client from spec in repo
    
    * Generate vardef client with valid return models
    
    * Tests deserialize to models

--- src/dapla_metadata/variable_definitions/generated/vardef_client/models/rendered_contact.py
+++ src/dapla_metadata/variable_definitions/generated/vardef_client/models/rendered_contact.py
@@ -12,23 +12,81 @@ Do not edit the class manually.
 from __future__ import annotations
 
 import json
-from enum import Enum
+import pprint
+import re  # noqa: F401
+from typing import Any
+from typing import ClassVar
 
+from pydantic import BaseModel
+from pydantic import ConfigDict
+from pydantic import StrictStr
 from typing_extensions import Self
 
 
-class VariableStatus(str, Enum):
-    """Life cycle status of a variable definition."""
+class RenderedContact(BaseModel):
+    """RenderedContact"""
 
-    """
-    allowed enum values
-    """
-    DRAFT = "DRAFT"
-    PUBLISHED_INTERNAL = "PUBLISHED_INTERNAL"
-    PUBLISHED_EXTERNAL = "PUBLISHED_EXTERNAL"
-    DEPRECATED = "DEPRECATED"
+    title: StrictStr | None = None
+    email: StrictStr
+    __properties: ClassVar[list[str]] = ["title", "email"]
+
+    model_config = ConfigDict(
+        populate_by_name=True,
+        validate_assignment=True,
+        protected_namespaces=(),
+    )
+
+    def to_str(self) -> str:
+        """Returns the string representation of the model using alias"""
+        return pprint.pformat(self.model_dump(by_alias=True))
+
+    def to_json(self) -> str:
+        """Returns the JSON representation of the model using alias"""
+        # TODO: pydantic v2: use .model_dump_json(by_alias=True, exclude_unset=True) instead
+        return json.dumps(self.to_dict())
 
     @classmethod
-    def from_json(cls, json_str: str) -> Self:
-        """Create an instance of VariableStatus from a JSON string"""
-        return cls(json.loads(json_str))
+    def from_json(cls, json_str: str) -> Self | None:
+        """Create an instance of RenderedContact from a JSON string"""
+        return cls.from_dict(json.loads(json_str))
+
+    def to_dict(self) -> dict[str, Any]:
+        """Return the dictionary representation of the model using alias.
+
+        This has the following differences from calling pydantic's
+        `self.model_dump(by_alias=True)`:
+
+        * `None` is only added to the output dict for nullable fields that
+          were set at model initialization. Other fields with value `None`
+          are ignored.
+        """
+        excluded_fields: set[str] = set([])
+
+        _dict = self.model_dump(
+            by_alias=True,
+            exclude=excluded_fields,
+            exclude_none=True,
+        )
+        # set to None if title (nullable) is None
+        # and model_fields_set contains the field
+        if self.title is None and "title" in self.model_fields_set:
+            _dict["title"] = None
+
+        return _dict
+
+    @classmethod
+    def from_dict(cls, obj: dict[str, Any] | None) -> Self | None:
+        """Create an instance of RenderedContact from a dict"""
+        if obj is None:
+            return None
+
+        if not isinstance(obj, dict):
+            return cls.model_validate(obj)
+
+        _obj = cls.model_validate(
+            {
+                "title": obj.get("title"),
+                "email": obj.get("email"),
+            }
+        )
+        return _obj
