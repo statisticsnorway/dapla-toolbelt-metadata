ed6d4220f8ca7d8d6c6275c8a0bdfb782d2225a2
Cecilie Seim <68303562+tilen1976@users.noreply.github.com>
2024-12-04 16:15:56 +0100

    update exception

--- src/dapla_metadata/variable_definitions/vardef/errors.py
+++ src/dapla_metadata/variable_definitions/vardef/errors.py
@@ -5,21 +5,20 @@ from dapla_metadata.variable_definitions.generated.vardef_client.exceptions impo
 )
 
 
+# validation exception
 class VardefClientException(OpenApiException):
     """Class for handling exceptions from Vardef."""
 
-    def __init__(self, message, code=None):
+    def __init__(self, message: str, code: str) -> None:
+        """Inherit from OpenApiException."""
         super().__init__(message)
         self.message = message
         self.code = code
 
-    def __str__(self):
-        # Formatted error message
-        if self.code:
-            data = json.loads(self.message)
-            error = data["message"]
-            embedded = data["_embedded"]
-            messages = [error["message"] for error in embedded["errors"]]
-            return f"{error}: {messages}"
-        return self.message
-
+    def __str__(self) -> str:
+        """Custom message."""
+        data = json.loads(self.message)
+        error = data["message"]
+        embedded = data["_embedded"]
+        messages = [error["message"] for error in embedded["errors"]]
+        return f"{error}: {messages}"
