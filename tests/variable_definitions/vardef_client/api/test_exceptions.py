f5840d3149731bf0e59c0c54767360639eb80eeb
Cecilie Seim <68303562+tilen1976@users.noreply.github.com>
2024-12-05 21:39:38 +0100

    Test

---
+++ tests/variable_definitions/vardef_client/api/test_exceptions.py
@@ -0,0 +1,44 @@
+"""Simple tests for exceptions."""
+
+from datetime import date
+
+import pytest
+
+from dapla_metadata.variable_definitions.generated.vardef_client.models import (
+    language_string_type,
+)
+from dapla_metadata.variable_definitions.generated.vardef_client.models.draft import (
+    Draft,
+)
+from dapla_metadata.variable_definitions.vardef.exceptions import VardefClientException
+from dapla_metadata.variable_definitions.vardef.vardef import Vardef
+
+draft_unittypes = Draft(
+    name=language_string_type.LanguageStringType(
+        nb=None,
+        nn=None,
+        en=None,
+    ),
+    short_name="test",
+    definition=language_string_type.LanguageStringType(
+        nb=None,
+        nn=None,
+        en=None,
+    ),
+    classification_reference="91",
+    unit_types=["a"],
+    subject_fields=["al"],
+    contains_sensitive_personal_information=True,
+    measurement_type="01",
+    valid_from=date(2024, 11, 1),
+    external_reference_uri="http://www.example.com",
+    comment=None,
+    related_variable_definition_uris=["http://www.example.com"],
+    contact=None,
+)
+
+
+def test_create_draft():
+    # api_instance = vardef_client.DraftVariableDefinitionsApi(api_client)
+    with pytest.raises(VardefClientException):
+        Vardef.create_draft(draft_unittypes)
