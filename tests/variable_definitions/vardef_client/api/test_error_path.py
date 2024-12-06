0bf1efcb95c278c7790838ef676e06147bdd235c
Cecilie Seim <68303562+tilen1976@users.noreply.github.com>
2024-12-06 14:44:28 +0100

    add test exception message

--- tests/variable_definitions/vardef_client/api/test_error_path.py
+++ tests/variable_definitions/vardef_client/api/test_error_path.py
@@ -1,10 +1,15 @@
 """Simple tests for exceptions."""
 
+import json
+
 import pytest
 
 from dapla_metadata.variable_definitions.generated import vardef_client
 from dapla_metadata.variable_definitions.generated.vardef_client.exceptions import (
     ApiException,
+)
+from dapla_metadata.variable_definitions.generated.vardef_client.exceptions import (
+    BadRequestException,
 )
 from tests.utils.constants import VARDEF_EXAMPLE_ACTIVE_GROUP
 
@@ -15,4 +20,23 @@ def test_create_draft(api_client, draft_invalid_unit_types):
         api_instance.create_variable_definition(
             VARDEF_EXAMPLE_ACTIVE_GROUP,
             draft_invalid_unit_types,
+        )
+
+
+def test_error_message_create_draft(api_client, draft_invalid_unit_types):
+    api_instance = vardef_client.DraftVariableDefinitionsApi(api_client)
+    try:
+        api_instance.create_variable_definition(
+            VARDEF_EXAMPLE_ACTIVE_GROUP,
+            draft_invalid_unit_types,
+        )
+    except BadRequestException as e:
+        response = e.body
+        data = json.loads(response)
+        embedded = data["_embedded"]
+        message = embedded["errors"]["message"]
+
+        assert (
+            message
+            == "draft.unitTypes[0]: Code a is not a member of classification with id 702"
         )
