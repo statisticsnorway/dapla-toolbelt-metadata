ad7d303b456492dce821f39f39ecf49d99129a83
Cecilie Seim <68303562+tilen1976@users.noreply.github.com>
2024-12-09 14:17:49 +0100

    Remove bad request example - add not found variable definition example and test

--- tests/variable_definitions/vardef_client/api/test_error_path.py
+++ tests/variable_definitions/vardef_client/api/test_error_path.py
@@ -6,56 +6,17 @@ import pytest
 
 from dapla_metadata.variable_definitions.generated import vardef_client
 from dapla_metadata.variable_definitions.generated.vardef_client.exceptions import (
-    BadRequestException,
-)
-from dapla_metadata.variable_definitions.generated.vardef_client.exceptions import (
     OpenApiException,
 )
-from tests.utils.constants import VARDEF_EXAMPLE_ACTIVE_GROUP
-from tests.utils.constants import VARDEF_EXAMPLE_DEFINITION_ID
 
 
-def test_create_draft(api_client, draft_invalid_unit_types):
-    api_instance = vardef_client.DraftVariableDefinitionsApi(api_client)
-    with pytest.raises(BadRequestException):
-        api_instance.create_variable_definition(
-            VARDEF_EXAMPLE_ACTIVE_GROUP,
-            draft_invalid_unit_types,
-        )
-
-
-def test_error_message_create_draft(api_client, draft_invalid_unit_types):
-    api_instance = vardef_client.DraftVariableDefinitionsApi(api_client)
-    try:
-        api_instance.create_variable_definition(
-            VARDEF_EXAMPLE_ACTIVE_GROUP,
-            draft_invalid_unit_types,
-        )
-    except BadRequestException as e:
-        response = e.body
-        data = json.loads(response)
-        embedded = data["_embedded"]
-        message = embedded["errors"]
-        last = message[0]["message"]
-
-        assert (
-            last
-            == "draft.unitTypes[0]: Code a is not a member of classification with id 702"
-        )
-
-
-def test_none_existing_patch(api_client):
-    api_instance = vardef_client.PatchesApi(api_client)
-    with pytest.raises(OpenApiException):
-        api_instance.get_patch(VARDEF_EXAMPLE_DEFINITION_ID, 200)
-
-
-def test_none_existing_patch_message(api_client):
-    api_instance = vardef_client.PatchesApi(api_client)
-    try:
-        api_instance.get_patch(VARDEF_EXAMPLE_DEFINITION_ID, 200)
-    except OpenApiException as e:
-        assert (
-            str(e.body)
-            == "The response /patch-id=200/variable-definition-id=wypvb3wd does not exist!"
-        )
+def test_not_found_variable(api_client):
+    api_instance = vardef_client.VariableDefinitionsApi(api_client)
+    with pytest.raises(
+        OpenApiException,
+        match="Variable with ID invalid id not found",
+    ) as exc_info:
+        api_instance.get_variable_definition_by_id("invalid id")
+    response = exc_info.value.body
+    result = json.loads(response)
+    assert result["detail"] == "Variable with ID invalid id not found"
