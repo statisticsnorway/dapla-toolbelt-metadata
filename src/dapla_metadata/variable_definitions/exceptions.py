474fd98645028377e5bff1a00a76eaf433eecad0
Cecilie Seim <68303562+tilen1976@users.noreply.github.com>
2024-12-16 09:12:48 +0100

    Format exception response (#77)
    * testing possibilities
    
    * format content of violations as string
    
    * fix test
    
    * linting
    
    * constraint violations as string

--- src/dapla_metadata/variable_definitions/exceptions.py
+++ src/dapla_metadata/variable_definitions/exceptions.py
@@ -39,13 +39,11 @@ class VardefClientException(OpenApiException):
             self.status = data.get("status", "Unknown status")
             if data.get("title") == "Constraint Violation":
                 violations = data.get("violations", [])
-                self.detail = [
-                    {
-                        "field": violation.get("field", "Unknown field"),
-                        "message": violation.get("message", "No message provided"),
-                    }
+                self.detail = "".join(
+                    f"\n{violation.get('field', 'Unknown field')}: {violation.get('message', 'No message provided')}"
                     for violation in violations
-                ]
+                )
+
             else:
                 self.detail = data.get("detail", "No detail provided")
             self.response_body = response_body
