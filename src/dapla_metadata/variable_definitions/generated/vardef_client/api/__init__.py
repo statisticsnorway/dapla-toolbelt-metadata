a205b0775c6d1ce4af6bf8f79f9e284502f33f95
Cecilie Seim <68303562+tilen1976@users.noreply.github.com>
2025-01-08 20:34:04 +0100

    make file

--- src/dapla_metadata/variable_definitions/generated/vardef_client/api/__init__.py
+++ src/dapla_metadata/variable_definitions/generated/vardef_client/api/__init__.py
@@ -1,9 +1,10 @@
 # flake8: noqa
 
 # import apis into api package
-from ..api.data_migration_api import DataMigrationApi
-from ..api.draft_variable_definitions_api import DraftVariableDefinitionsApi
-from ..api.patches_api import PatchesApi
-from ..api.public_api import PublicApi
-from ..api.validity_periods_api import ValidityPeriodsApi
-from ..api.variable_definitions_api import VariableDefinitionsApi
+from vardef_client.api.data_migration_api import DataMigrationApi
+from vardef_client.api.draft_variable_definitions_api import DraftVariableDefinitionsApi
+from vardef_client.api.patches_api import PatchesApi
+from vardef_client.api.public_api import PublicApi
+from vardef_client.api.validity_periods_api import ValidityPeriodsApi
+from vardef_client.api.variable_definitions_api import VariableDefinitionsApi
+
