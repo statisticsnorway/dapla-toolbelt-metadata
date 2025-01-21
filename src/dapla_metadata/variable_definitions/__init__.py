ada6eee9d20b874712d3e5caa5446be4707cebca
Miles Mason Winther <42948872+mmwinther@users.noreply.github.com>
2025-01-21 15:03:36 +0100

    Simplify imports in vardef demo notebook

--- src/dapla_metadata/variable_definitions/__init__.py
+++ src/dapla_metadata/variable_definitions/__init__.py
@@ -1,7 +1,7 @@
 """Client for working with Variable Definitions at Statistics Norway."""
 
+from .generated.vardef_client import models
 from .generated.vardef_client.exceptions import *  # noqa: F403
-from .generated.vardef_client.models import *  # noqa: F403
 from .vardef import Vardef
 from .variable_definition import CompletePatchOutput
 from .variable_definition import VariableDefinition
