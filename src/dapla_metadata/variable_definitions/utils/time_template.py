286d8e31f08eb7d8bc94349b45cc551f26be9977
Cecilie Seim <68303562+tilen1976@users.noreply.github.com>
2025-02-20 18:39:23 +0100

    fileformat datetime

--- src/dapla_metadata/variable_definitions/utils/time_template.py
+++ src/dapla_metadata/variable_definitions/utils/time_template.py
@@ -1,7 +1,10 @@
 from datetime import datetime
 
+import pytz
+
 
 def get_current_time() -> str:
     """Return a string format date now for filename."""
-    current_datetime = datetime.now(tz="Europe/Oslo").strftime("%Y-%m-%d_%H-%M-%S")
+    timezone = pytz.timezone("Europe/Oslo")
+    current_datetime = datetime.now(timezone).strftime("%Y-%m-%dT%H-%M-%S")
     return str(current_datetime)
