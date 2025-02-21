2d9bfa74c4e8776800539a95d75106d45aa12521
Cecilie Seim <68303562+tilen1976@users.noreply.github.com>
2025-02-21 13:12:50 +0100

    improve docstrings test

--- tests/variable_definitions/utils/test_template.py
+++ tests/variable_definitions/utils/test_template.py
@@ -8,23 +8,19 @@ from dapla_metadata.variable_definitions.utils.constants import DEFAULT_TEMPLATE
 from dapla_metadata.variable_definitions.utils.template import (
     model_to_yaml_with_comments,
 )
-from dapla_metadata.variable_definitions.utils.time_template import get_current_time
 from dapla_metadata.variable_definitions.variable_definition import CompletePatchOutput
 
 yaml = ruamel.yaml.YAML()
 
 
 def test_yaml_file_creation():
-    """Test if the function generates a YAML file and returns the correct path."""
-    model_to_yaml_with_comments(DEFAULT_TEMPLATE)
-
-    time_stamp = get_current_time()
-    output_file = Path("variable_definition_template_" + time_stamp + ".yaml")
-    assert output_file.exists(), "YAML file was not created"
+    """Test if the function generates a YAML file."""
+    file_path = model_to_yaml_with_comments(DEFAULT_TEMPLATE)
+    assert file_path.exists(), "YAML file was not created"
 
 
 def test_yaml_content_default_values():
-    """Check if the generated YAML file contains the expected data and structure."""
+    """Check if the generated YAML file with default values contains the expected data."""
     file_path = model_to_yaml_with_comments(DEFAULT_TEMPLATE)
     with Path.open(file_path, encoding="utf-8") as f:
         parsed_yaml = yaml.load(f)
@@ -34,7 +30,7 @@ def test_yaml_content_default_values():
 
 
 def test_yaml_content_saved_values(complete_patch_output: CompletePatchOutput) -> None:
-    """Check if the generated YAML file contains the expected data and structure."""
+    """Check if the generated YAML file with saved values contains the expected data."""
     file_path = model_to_yaml_with_comments(complete_patch_output)
     with Path.open(file_path, encoding="utf-8") as f:
         parsed_yaml = yaml.load(f)
@@ -44,20 +40,19 @@ def test_yaml_content_saved_values(complete_patch_output: CompletePatchOutput) -
 
 
 def test_yaml_comments():
-    """Ensure the YAML file includes the expected comments in the right sections."""
+    """Ensure the YAML file includes the expected header comments."""
     file_path = model_to_yaml_with_comments(DEFAULT_TEMPLATE)
 
     with Path.open(file_path, encoding="utf-8") as f:
         content = f.read()
 
-    # Check for expected section headers as comments
     assert "--- Variable definition template ---" in content
     assert "--- Status field" in content
     assert "--- Owner team" in content
     assert "--- Machine generated fields" in content
-    # other comments on field
 
 
 def test_file_name():
+    """Check filename."""
     file_path = model_to_yaml_with_comments(DEFAULT_TEMPLATE)
     assert "variable_definition_template_" in str(file_path)
