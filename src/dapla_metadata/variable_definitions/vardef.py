4dfd8e496aa8605ce55a18f1610196bbaff1744e
Cecilie Seim <68303562+tilen1976@users.noreply.github.com>
2025-02-21 13:48:50 +0100

    remove try catch

--- src/dapla_metadata/variable_definitions/vardef.py
+++ src/dapla_metadata/variable_definitions/vardef.py
@@ -178,10 +178,8 @@ class Vardef:
     @classmethod
     def write_template_to_file(cls) -> str:
         """Write template with default values to a yaml file."""
-        try:
-            file_path = model_to_yaml_with_comments()
-            # Check if the file was created
-            if file_path.exists():
-                return "Successfully written to file"
-        except TypeError as e:
-            return f"Error: {e!s}"
+        file_path = model_to_yaml_with_comments()
+        # Check if the file was created
+        if file_path.exists():
+            return "Successfully written to file"
+        return ""
