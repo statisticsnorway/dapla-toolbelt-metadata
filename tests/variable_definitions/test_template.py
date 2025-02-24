4427babc28611de342ffaffd1852bedefee2f974
Cecilie Seim <68303562+tilen1976@users.noreply.github.com>
2025-02-24 12:42:57 +0100

    update tests to use tmp_path

--- tests/variable_definitions/test_template.py
+++ tests/variable_definitions/test_template.py
@@ -1,48 +1,39 @@
 """Test class for creating a yaml template."""
 
+from pathlib import Path
 
 import ruamel.yaml
-
-from dapla_metadata.variable_definitions.utils.constants import DEFAULT_TEMPLATE
-from dapla_metadata.variable_definitions.utils.template import (
-    model_to_yaml_with_comments,
-)
-from dapla_metadata.variable_definitions.variable_definition import CompletePatchOutput
 
 yaml = ruamel.yaml.YAML()
 
 
-def test_yaml_file_creation():
+def test_yaml_file_creation(work_folder_defaults: Path):
     """Test if the function generates a YAML file."""
-    file_path = model_to_yaml_with_comments(DEFAULT_TEMPLATE)
+    file_path = work_folder_defaults
     assert file_path.exists(), "YAML file was not created"
 
 
-def test_yaml_content_default_values():
+def test_yaml_content_default_values(work_folder_defaults: Path):
     """Check if the generated YAML file with default values contains the expected data."""
-    file_path = model_to_yaml_with_comments(DEFAULT_TEMPLATE)
-    with file_path.open(encoding="utf-8") as f:
+    with work_folder_defaults.open(encoding="utf-8") as f:
         parsed_yaml = yaml.load(f)
 
     assert parsed_yaml["variable_status"] == "DRAFT"
     assert parsed_yaml["owner"]["team"] == "default team"
 
 
-def test_yaml_content_saved_values(complete_patch_output: CompletePatchOutput) -> None:
+def test_yaml_content_saved_values(work_folder_saved_variable: Path) -> None:
     """Check if the generated YAML file with saved values contains the expected data."""
-    file_path = model_to_yaml_with_comments(complete_patch_output)
-    with file_path.open(encoding="utf-8") as f:
+    with work_folder_saved_variable.open(encoding="utf-8") as f:
         parsed_yaml = yaml.load(f)
 
     assert parsed_yaml["variable_status"] == "PUBLISHED_INTERNAL"
     assert parsed_yaml["last_updated_by"] == "ano@ssb.no"
 
 
-def test_yaml_comments():
+def test_yaml_comments(work_folder_defaults: Path):
     """Ensure the YAML file includes the expected header comments."""
-    file_path = model_to_yaml_with_comments(DEFAULT_TEMPLATE)
-
-    with file_path.open(encoding="utf-8") as f:
+    with work_folder_defaults.open(encoding="utf-8") as f:
         content = f.read()
 
     assert "--- Variable definition template ---" in content
@@ -51,7 +42,6 @@ def test_yaml_comments():
     assert "--- Machine generated fields" in content
 
 
-def test_file_name():
+def test_file_name(work_folder_defaults: Path):
     """Check filename."""
-    file_path = model_to_yaml_with_comments(DEFAULT_TEMPLATE)
-    assert "variable_definition_template_" in str(file_path)
+    assert "variable_definition_template_" in str(work_folder_defaults)
