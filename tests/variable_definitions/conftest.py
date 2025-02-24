7ad6c2842284f9e97018fab1092e1010aae00edd
Cecilie Seim <68303562+tilen1976@users.noreply.github.com>
2025-02-24 12:42:38 +0100

    add fixture for tmp_paths

--- tests/variable_definitions/conftest.py
+++ tests/variable_definitions/conftest.py
@@ -1,5 +1,6 @@
 from collections.abc import Generator
 from datetime import date
+from pathlib import Path
 
 import pytest
 
@@ -33,6 +34,10 @@ from dapla_metadata.variable_definitions.generated.vardef_client.models.validity
 )
 from dapla_metadata.variable_definitions.generated.vardef_client.models.variable_status import (
     VariableStatus,
+)
+from dapla_metadata.variable_definitions.utils.constants import DEFAULT_TEMPLATE
+from dapla_metadata.variable_definitions.utils.template import (
+    model_to_yaml_with_comments,
 )
 from dapla_metadata.variable_definitions.variable_definition import CompletePatchOutput
 from dapla_metadata.variable_definitions.variable_definition import VariableDefinition
@@ -231,3 +236,44 @@ def sample_complete_patch_output() -> CompletePatchOutput:
 @pytest.fixture
 def complete_patch_output() -> CompletePatchOutput:
     return sample_complete_patch_output()
+
+
+def _clean_up_after_test(target_path: Path, base_path: Path):
+    if target_path.exists():
+        target_path.unlink()
+    if base_path.exists():
+        base_path.rmdir()
+
+
+@pytest.fixture
+def work_folder_defaults(tmp_path: Path):
+    """Fixture that ensures a work folder exists for template with default values."""
+    base_path = tmp_path / "work"
+    base_path.mkdir(parents=True, exist_ok=True)
+
+    file_name = model_to_yaml_with_comments(
+        DEFAULT_TEMPLATE,
+        custom_directory=base_path,
+    )
+
+    target_path = base_path / file_name
+
+    yield target_path
+
+    _clean_up_after_test(target_path, base_path)
+
+
+@pytest.fixture
+def work_folder_saved_variable(tmp_path: Path):
+    """Fixture that ensures a work folder exists for template with saved variable definition values."""
+    base_path = tmp_path / "work"
+    base_path.mkdir(parents=True, exist_ok=True)
+
+    file_name = model_to_yaml_with_comments(
+        sample_complete_patch_output(),
+        custom_directory=base_path,
+    )
+    target_path = base_path / file_name
+    yield target_path
+
+    _clean_up_after_test(target_path, base_path)
