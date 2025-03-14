d71b4668f936503179562fe35c10d2a1c1ecb88e
Miles Mason Winther <42948872+mmwinther@users.noreply.github.com>
2025-03-14 13:26:04 +0100

    Ignore warning from google library

--- src/dapla_metadata/__init__.py
+++ src/dapla_metadata/__init__.py
@@ -1,5 +1,12 @@
 """Tools and clients for working with the Dapla Metadata system."""
 
+import warnings
+
+warnings.filterwarnings(
+    "ignore",
+    message="As the c extension couldn't be imported, `google-crc32c` is using a pure python implementation that is significantly slower.",
+)
+
 import datadoc_model.model as datadoc_model
 
 from . import datasets
