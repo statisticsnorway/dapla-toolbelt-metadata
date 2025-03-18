12d751a83098116e9fce1b6aaf76247c63f91ab7
Cecilie Seim <68303562+tilen1976@users.noreply.github.com>
2025-03-18 21:50:18 +0100

    update types

--- src/dapla_metadata/dapla/standards.py
+++ src/dapla_metadata/dapla/standards.py
@@ -1,9 +1,10 @@
 from os import PathLike
+import os
 from pathlib import Path
 from cloudpathlib import CloudPath
 from dapla_metadata.dapla.name_validator import NameStandardValidator
 
-def check_naming_standard(file_path: Path | CloudPath | PathLike) -> str | list:
+def check_naming_standard(file_path: str | os.PathLike[str]) -> str | list:
     """Check a given path."""
     naming_validator = NameStandardValidator(file_path)
     return naming_validator.validate
