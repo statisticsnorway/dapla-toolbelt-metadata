cb3034987679e106b355110a828f5f556083e471
Cecilie Seim <68303562+tilen1976@users.noreply.github.com>
2025-03-19 22:40:56 +0100

    update type

--- src/dapla_metadata/standards/standard_validators.py
+++ src/dapla_metadata/standards/standard_validators.py
@@ -1,9 +1,7 @@
-import os
-
 from dapla_metadata.standards.name_validator import NameStandardValidator
 
 
-def check_naming_standard(file_path: str | os.PathLike[str]) -> str | list:
+def check_naming_standard(file_path: str) -> str | list:
     """Check a given path following ssb name standard."""
     naming_validator = NameStandardValidator(file_path)
     return naming_validator.validate
