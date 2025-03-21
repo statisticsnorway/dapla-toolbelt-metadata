3a5a86d5232dc06dc8bbf50ca324f3d813160442
Cecilie Seim <68303562+tilen1976@users.noreply.github.com>
2025-03-21 22:57:46 +0100

    add test

--- tests/standards/test_name_validator.py
+++ tests/standards/test_name_validator.py
@@ -1,3 +1,5 @@
+import os
+
 import pytest
 
 from dapla_metadata.standards.name_validator import NAME_STANDARD_SUCSESS
@@ -31,9 +33,19 @@ def test_symbols_in_filepath(data: str, expected_result: bool):
 
 def test_validate_directory():
     """Test recursive directory validation."""
+    os.chdir("tests/standards/buckets")
     validator = NameStandardValidator(
         file_path=None,
-        bucket_name="tests/standards/buckets/ssb-staging",
+        bucket_name="ssb-staging",
     )
     results = validator.validate_bucket()
     assert results[0][1] == NAME_STANDARD_SUCSESS
+
+
+def test_validate_directory_2():
+    validator = NameStandardValidator(
+        file_path="tests/standards/buckets/ssb-staging/datadoc/utdata/person_data_p2021_v2.parquet",
+        bucket_name=None,
+    )
+    results = validator.validate()
+    assert results == NAME_STANDARD_SUCSESS
