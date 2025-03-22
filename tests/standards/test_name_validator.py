451ced9b28fd4c95ea2a5fe265478e5f4b62f6b9
Cecilie Seim <68303562+tilen1976@users.noreply.github.com>
2025-03-22 11:32:04 +0100

    fix tests

--- tests/standards/test_name_validator.py
+++ tests/standards/test_name_validator.py
@@ -2,6 +2,7 @@ import os
 
 import pytest
 
+from dapla_metadata.standards.name_validator import MISSING_DATASET_SHORT_NAME
 from dapla_metadata.standards.name_validator import NAME_STANDARD_SUCSESS
 from dapla_metadata.standards.name_validator import NameStandardValidator
 
@@ -33,10 +34,10 @@ def test_symbols_in_filepath(data: str, expected_result: bool):
 
 def test_validate_directory():
     """Test recursive directory validation."""
-    os.chdir("tests/standards/buckets")
+    os.chdir("tests/standards/resources/buckets")
     validator = NameStandardValidator(
         file_path=None,
-        bucket_name="ssb-staging",
+        bucket_name="produkt",
     )
     results = validator.validate_bucket()
     assert results[0][1] == NAME_STANDARD_SUCSESS
@@ -44,8 +45,17 @@ def test_validate_directory():
 
 def test_validate_directory_2():
     validator = NameStandardValidator(
-        file_path="tests/standards/buckets/ssb-staging/datadoc/utdata/person_data_p2021_v2.parquet",
+        file_path="tests/standards/resources/buckets/produkt/stat/inndata/person_data_p2021_v2.parquet",
         bucket_name=None,
     )
     results = validator.validate()
     assert results == NAME_STANDARD_SUCSESS
+
+
+def test_validate_directory_3():
+    validator = NameStandardValidator(
+        file_path="tests/standards/resources/buckets/produkt/stat/inndata/p2021_v2.parquet",
+        bucket_name=None,
+    )
+    results = validator.validate()
+    assert results == [MISSING_DATASET_SHORT_NAME]
--- tests/standards/test_standards.py
+++ tests/standards/test_standards.py
@@ -10,125 +10,82 @@ from dapla_metadata.standards.name_validator import PATH_IGNORED
 from dapla_metadata.standards.standard_validators import check_naming_standard
 
 
-@pytest.mark.parametrize(
-    ("data"),
-    [
-        "gs://ssb-staging-dapla-felles-data-delt/datadoc/utdata/person_data_p2021_v2.parquet",
-        "gs://ssb-staging-dapla-felles-data-delt/datadoc/utdata/person_data_p2021_p2022_v2.parquet",
-        "gs://ssb-staging-dapla-felles-data-delt/datadoc/utdata/undermappe/person_data_p2021_v2.parquet",
-        "buckets/bucket_name/dataset/klargjorte_data/arbmark/resources/person_data_p2021-12-31_p2021-12-31_v1.parquet",
-        "stat/inndata/person_testdata_p2021-12-31_p2021-12-31_v1.parquet",
-        "bucket_name/stat/klargjorte-data/person_testdata_p2021-12-31_p2021-12-31_v1.parquet",
-        "ssb-dapla-example-data-produkt-prod/ledstill/inndata/skjema_p2018_p2020_v1",
-        "/buckets/produkt/datadoc/brukertest/1/sykefratot/klargjorte_data/person_testdata_p2021-12-31_p2021-12-31_v1.parquet",
-        "buckets/produkt/datadoc/brukertest/1/sykefratot/klargjorte_data/person_testdata_p2021-12-31_p2021-12-31_v1.json",
-    ],
-)
-def test_valid_names(data: str):
-    assert check_naming_standard(file_path=data) == NAME_STANDARD_SUCSESS
+def test_valid_path(setup_test_files):
+    test_files, tmp_path = setup_test_files
+    for test_file in test_files:
+        assert test_file.exists(), f"File {test_file} does not exist."
+        assert check_naming_standard(test_file) == NAME_STANDARD_SUCSESS
+
+
+def test_missing_date_period(setup_test_files_period):
+    test_files, tmp_path = setup_test_files_period
+    for test_file in test_files:
+        assert test_file.exists(), f"File {test_file} does not exist."
+        assert check_naming_standard(file_path=test_file) == [MISSING_PERIOD]
+
+
+def test_missing_data_state(setup_test_files_state):
+    test_files, tmp_path = setup_test_files_state
+    for test_file in test_files:
+        assert test_file.exists(), f"File {test_file} does not exist."
+        assert check_naming_standard(file_path=test_file) == MISSING_DATA_STATE
 
 
 @pytest.mark.parametrize(
-    ("data"),
-    [
-        "gs://ssb-staging-dapla-felles-data-delt/datadoc/utdata/person_data_v1.parquet",
-        "tests/dataset/klargjorte_data/arbmark/resources/person_data_v1.parquet",
-    ],
-)
-def test_missing_date_period(data: str):
-    assert check_naming_standard(file_path=data) == [MISSING_PERIOD]
-
-
-@pytest.mark.parametrize(
-    ("data"),
-    [
-        "gs://ssb-staging-dapla-felles-data-delt/stat_reg/person_data_p2022_v1.parquet",
-        "gs://ssb-staging-dapla-felles-data-delt/datadoc/person_data_p2021_v3.parquet",
-        "buckets/produkt/test-2/person_testdata_p2021-12-31_p2021-12-31_v1.parquet",
-    ],
-)
-def test_missing_required_data_state(data: str):
-    assert check_naming_standard(file_path=data) == MISSING_DATA_STATE
-
-
-@pytest.mark.parametrize(
-    ("data"),
+    ("file_path"),
     [
         "gs://ssb-staging-dapla-felles-data-delt/inndata/person_data_p2022_v1.parquet",
-        "gs://ssb-staging-dapla-felles-data-delt/klargjorte-data/person_data_p2021_v3.parquet",
-        "buckets/produkt/utdata/person_testdata_p2021-12-31_p2021-12-31_v1.parquet",
-        "utdata/person_testdata_p2021-12-31_p2021-12-31_v1.parquet",
     ],
 )
-def test_missing_shortname_folder(data: str):
-    assert check_naming_standard(file_path=data) == [MISSING_SHORT_NAME]
+def test_missing_shortname(file_path, tmp_path):
+    test_file = tmp_path / file_path
+    test_file.parent.mkdir(parents=True, exist_ok=True)
+    test_file.touch()
+    assert check_naming_standard(file_path=test_file) == [MISSING_SHORT_NAME]
+
+
+def test_inored_paths(setup_test_files_ignored):
+    test_files, tmp_path = setup_test_files_ignored
+    for test_file in test_files:
+        assert test_file.exists(), f"File {test_file} does not exist."
+        assert check_naming_standard(file_path=test_file) == PATH_IGNORED
+
+
+def test_invalid_symbols(setup_test_files_invalid_symbols):
+    test_files, tmp_path = setup_test_files_invalid_symbols
+    for test_file in test_files:
+        assert test_file.exists(), f"File {test_file} does not exist."
+        assert check_naming_standard(file_path=test_file) == [INVALID_SYMBOLS]
+
+
+def test_missing_dataset_shortname(setup_test_files_dataset_shortname):
+    test_files, tmp_path = setup_test_files_dataset_shortname
+    for test_file in test_files:
+        assert test_file.exists(), f"File {test_file} does not exist."
+        assert check_naming_standard(file_path=test_file) == [
+            MISSING_DATASET_SHORT_NAME,
+        ]
 
 
 @pytest.mark.parametrize(
-    ("data"),
-    [
-        "buckets/ssb-dapla-example-data-produkt-prod/ledstill/temp/skjema_v1.parquet",
-        "gs:/ssb-dapla-example-data-produkt-prod/ledstill/temp/skjema_v1.parquet",
-        "buckets/ssb-dapla-example-data-produkt-prod/ledstill/inndata/temp/skjema_p2018_p2020_v2.parquet",
-        "gs://ssb-dapla-example-data-produkt-prod/temp/ledstill/inndata/temp/skjema_p2018_p2020",
-        "gs://ssb-dapla-example-data-produkt-prod/konfigurasjon/ledstill/inndata/skjema_p2018_p2020",
-        "gs://ssb-dapla-example-data-produkt-prod/Konfigurasjon/ledstill/inndata/skjema_p2018_p2020",
-    ],
-)
-def test_inored_paths(data: str):
-    assert check_naming_standard(file_path=data) == PATH_IGNORED
-
-
-@pytest.mark.parametrize(
-    ("data"),
-    [
-        "buckets/ssb-dapla-example-data-produkt-prod/ledstill/kildedata/skjema_v1",
-        "buckets/ssb-dapla-example-data-produkt-prod/ledstill/kildedata/skjema_p2018_p2020_v1",
-        "gs://ssb-dapla-example-data-produkt-prod/ledstill/kildedata/skjema_p2018_p2020_v1",
-    ],
-)
-def test_source_data_is_ignored(data: str):
-    assert check_naming_standard(file_path=data) == PATH_IGNORED
-
-
-@pytest.mark.parametrize(
-    ("data"),
-    [
-        "buckets/ssb-dapla-example-data-produkt-prod/ledstill/utdata/persån_testdata_p2021-12-31_p2021-12-31_v1.parquet",
-        "gs://ssb-dapla-example-data-prædukt-prod/ledstill/utdata/person_testdata_p2021-12-31_p2021-12-31_v1.parquet",
-    ],
-)
-def test_invalid_symbols(data: str):
-    assert check_naming_standard(file_path=data) == [INVALID_SYMBOLS]
-
-
-@pytest.mark.parametrize(
-    ("data"),
-    [
-        "gs://ssb-staging-dapla-felles-data-delt/datadoc/utdata/p2021_v2.parquet",
-        "buckets/produkt/datadoc/brukertest/1/sykefratot/klargjorte_data/_p2021-12-31_p2021-12-31_v1.json",
-    ],
-)
-def test_missing_dataset_shortname(data: str):
-    assert check_naming_standard(file_path=data) == [MISSING_DATASET_SHORT_NAME]
-
-
-@pytest.mark.parametrize(
-    ("data", "violations"),
+    ("file_path", "violations"),
     [
         (
             "gs://ssb-dapla-example-data-produkt-prod/inndata/skjema_v1.parquet",
             [MISSING_SHORT_NAME, MISSING_PERIOD],
         ),
         (
-            "klargjorte_data/_p2021-12-31_p2021-12-31_v1.parquet",
+            "buckets/klargjorte_data/_p2021-12-31_p2021-12-31_v1.parquet",
             [MISSING_SHORT_NAME, MISSING_DATASET_SHORT_NAME],
         ),
         (
-            "/klargjorte_data/_p2021-12-31_p2021-12-31_v1.parquet",
+            "gs://klargjorte_data/_p2021-12-31_p2021-12-31_v1.parquet",
             [MISSING_SHORT_NAME, MISSING_DATASET_SHORT_NAME],
         ),
     ],
 )
-def test_missing_multiple(data: str, violations: list):
-    assert check_naming_standard(file_path=data) == violations
+def test_missing_multiple(file_path: str, violations: list, tmp_path):
+    full_path = tmp_path / file_path
+    full_path.parent.mkdir(parents=True, exist_ok=True)
+    full_path.touch()
+    assert check_naming_standard(file_path=full_path) == violations
