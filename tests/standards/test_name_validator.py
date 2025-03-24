584da3abec386c0ac339ae8f6780cdec0b5f07d5
Cecilie Seim <68303562+tilen1976@users.noreply.github.com>
2025-03-24 13:27:12 +0100

    test name validator

---
+++ tests/standards/test_name_validator.py
@@ -0,0 +1,27 @@
+import pytest
+
+from dapla_metadata.standards.name_validator import NameStandardValidator
+from dapla_metadata.standards.utils.constants import NAME_STANDARD_SUCSESS
+
+
+@pytest.mark.parametrize(
+    ("file_path"),
+    [
+        "buckets/produkt/datadoc/utdata/person_data_p2021_v2.parquet",
+        "produkt/datadoc/utdata/person_data_p2021_p2022_v2.parquet",
+        "datadoc/utdata/undermappe/person_data_p2021_v2.parquet",
+        "delt-data/dataset/klargjorte_data/arbmark/resources/person_data_p2021-12-31_p2021-12-31_v1.parquet",
+        "stat/inndata/person_testdata_p2021-12-31_p2021-12-31_v1.parquet",
+        "ssb-delt/stat/klargjorte-data/person_testdata_p2021-12-31_p2021-12-31_v1.parquet",
+        "ssb-test/ledstill/inndata/skjema_p2018_p2020_v1",
+        "produkt-delt/datadoc/brukertest/1/sykefratot/klargjorte_data/person_testdata_p2021-12-31_p2021-12-31_v1.parquet",
+        "samfunns-produkt/datadoc/brukertest/1/sykefratot/klargjorte_data/person_testdata_p2021-12-31_p2021-12-31_v1.json",
+    ],
+)
+def test_validate_sucsess(file_path, tmp_path):
+    full_path = tmp_path / file_path
+    full_path.parent.mkdir(parents=True, exist_ok=True)
+    full_path.touch()
+    path_to_check = NameStandardValidator(file_path=full_path, bucket_name=None)
+    result = path_to_check.validate()
+    assert result.messages[0] == NAME_STANDARD_SUCSESS
