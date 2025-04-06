2643fb60c40a39af7e32ead81357ad5cbbf08ee9
Cecilie Seim <68303562+tilen1976@users.noreply.github.com>
2025-04-06 21:35:25 +0200

    undo multi representer

--- src/dapla_metadata/variable_definitions/_utils/files.py
+++ src/dapla_metadata/variable_definitions/_utils/files.py
@@ -199,7 +199,7 @@ def _configure_yaml() -> YAML:
     yaml.indent(
         mapping=4, sequence=4, offset=2
     )  # Ensure indentation for nested keys and lists
-    yaml.representer.add_multi_representer(str, represent_str)
+    yaml.representer.add_representer(str, represent_str)
     yaml.representer.add_representer(
         VariableStatus,
         lambda dumper, data: dumper.represent_scalar(
