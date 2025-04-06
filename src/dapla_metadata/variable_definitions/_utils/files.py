95cc6a4bfcba4ee504ee6774d6400a0c1d9b391c
Cecilie Seim <68303562+tilen1976@users.noreply.github.com>
2025-04-07 00:10:04 +0200

    remove new line

--- src/dapla_metadata/variable_definitions/_utils/files.py
+++ src/dapla_metadata/variable_definitions/_utils/files.py
@@ -182,9 +182,8 @@ def represent_str(dumper: RoundTripDumper, data: str):
     """Set."""
     if "\n" in data or len(data) > 180:
         # Use literal block style (|)
-        data = "\n".join([line.strip() for line in data.splitlines()])
         return dumper.represent_scalar(
-            "tag:yaml.org,2002:str", LiteralScalarString(data)
+            "tag:yaml.org,2002:str", LiteralScalarString(data.strip())
         )
     return dumper.represent_scalar("tag:yaml.org,2002:str", data, style='"')
 
