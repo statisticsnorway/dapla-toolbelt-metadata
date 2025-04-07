4495883e0f9897ffe1a21bdf1ba6918df1064a08
Miles Mason Winther <42948872+mmwinther@users.noreply.github.com>
2025-04-07 16:39:26 +0200

    Add ls for debugging

--- vardef-maintenance/vardef_client_init.sh
+++ vardef-maintenance/vardef_client_init.sh
@@ -4,11 +4,12 @@
 git clone -n --depth=1 --filter=tree:0 \
   https://github.com/statisticsnorway/dapla-toolbelt-metadata.git $HOME/tmp/dapla-toolbelt-metadata
 pushd "$HOME"/tmp/dapla-toolbelt-metadata || exit
-git sparse-checkout set --no-cone /demo/variable_definitions --no-cone vardef-maintenance/vardef-maintenance.toml
+git sparse-checkout set --no-cone /demo/variable_definitions --no-cone /vardef-maintenance/vardef-maintenance.toml
 git checkout
 cp -R demo/variable_definitions "$HOME"/work/variable_definitions
 cp vardef-maintenance/vardef-maintenance.toml "$HOME"/work/variable_definitions/pyproject.toml
 popd || exit
 rm -rf "$HOME"/tmp/dapla-toolbelt-metadata
 pushd "$HOME"/work/variable_definitions || exit
+ls -la .
 ssb-project build --no-verify
