bcc12045f16bc169b0bbf2d17b6cbad2107824c7
Cecilie Seim <68303562+tilen1976@users.noreply.github.com>
2025-04-07 12:48:50 +0200

    add comment

--- src/dapla_metadata/variable_definitions/_utils/files.py
+++ src/dapla_metadata/variable_definitions/_utils/files.py
@@ -201,7 +201,7 @@ def _configure_yaml() -> YAML:
     yaml.default_flow_style = False  # Ensures pretty YAML formatting block style
     yaml.allow_unicode = True  # Support special characters
     yaml.preserve_quotes = True
-    yaml.width = 180
+    yaml.width = 180  # wrap long lines
     yaml.indent(
         mapping=4, sequence=2, offset=0
     )  # Ensure indentation for nested keys and lists
