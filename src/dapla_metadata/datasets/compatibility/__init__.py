fb232df05c1f79a2d2272e55721cb7c13a5411b6
Miles Mason Winther <42948872+mmwinther@users.noreply.github.com>
2025-09-04 09:22:46 +0200

    Create compatibility package

---
+++ src/dapla_metadata/datasets/compatibility/__init__.py
@@ -0,0 +1,7 @@
+"""Model Backwards Compatibility.
+
+This package contains code for upgrading existing metadata documents to the newest version of the model.
+This is analogous to a Database Migration where the structure of the data has changed and we wish to
+retain already persisted information.
+
+"""
