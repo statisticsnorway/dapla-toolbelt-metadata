46684163851420d70bf3841c8b79641af0f40957
Jorgen-5 <jrussnes@gmail.com>
2025-11-20 15:46:44 +0100

    Update src/dapla_metadata/datasets/dapla_dataset_path_info.py
    Co-authored-by: Miles Mason Winther <42948872+mmwinther@users.noreply.github.com>

--- src/dapla_metadata/datasets/dapla_dataset_path_info.py
+++ src/dapla_metadata/datasets/dapla_dataset_path_info.py
@@ -334,7 +334,7 @@ def categorize_period_string(period: str) -> IsoDateFormat | SsbDateFormat:
 class DaplaDatasetPathInfo:
     """Extract info from a path following SSB's dataset naming convention."""
 
-    def __init__(self, dataset_path: str | os.PathLike[str] | UPath) -> None:
+    def __init__(self, dataset_path: ReadablePathLike) -> None:
         """Digest the path so that it's ready for further parsing."""
         self.dataset_string = str(dataset_path)
         self.dataset_path = UPath(self.dataset_string)
