350b83e5574164ecdb3acda22abbecfd0759cd31
Cecilie Seim <68303562+tilen1976@users.noreply.github.com>
2026-01-05 15:17:22 +0100

    name.nb can be None

--- src/dapla_metadata/variable_definitions/vardef.py
+++ src/dapla_metadata/variable_definitions/vardef.py
@@ -283,7 +283,7 @@ class Vardef:
                     render=False,
                 )
             ],
-            key=lambda p: p.name.nb,
+            key=lambda p: (p.name.nb or ""),
         )
 
     @classmethod
